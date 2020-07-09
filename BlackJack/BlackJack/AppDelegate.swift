d0c94ca6dfe1e561db0181977f7791a50d0f3cd7
Vaghula krishnan <vaghulk@gmail.com>
2020-07-09 18:23:58 -0400

    project setup

---
+++ BlackJack/BlackJack/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BlackJack
+//
+//  Created by Vaghula Krishnan on 2020-07-09.
+//  Copyright © 2020 Vaghula Krishnan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
